82d83e4c14cba97f1e12b13490284886a59af605
Murillo Ressineti <murillobr22@gmail.com>
2023-05-26 12:35:06 -0300

    Exercícios

--- curso/aula12/ex011.js
+++ curso/aula12/ex011.js
@@ -5,8 +5,8 @@ console.log(`Você tem: ${idade} anos`)
 if (idade < 16) {
     console.log('Não vota')
 
-} else if (idade < 18 || idade> 65 ) {     // || Significa "Ou"
+} else if (idade < 18 || idade > 65 ) {     // || Significa "Ou"
         console.log('Voto opcional')
-    } else {
+} else {
         console.log('Voto obrigatório')
     }
