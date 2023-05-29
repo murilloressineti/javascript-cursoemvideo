d9501e9026b3703ced699209c36cd498e9d1201a
murilloressineti <murillobr22@gmail.com>
2023-05-29 12:40:55 -0300

    ex012

--- curso/aula12/ex012.js
+++ curso/aula12/ex012.js
@@ -4,10 +4,10 @@ console.log(`Agora são exatamente ${hora} horas.`)
 
 if (hora < 6 ) {
     console.log('Boa madrugada!')
-} else if (hora <= 12) {
+} else if (hora < 12) {
     console.log('Bom dia!')
 }
-else if (hora <= 18) {
+else if (hora < 18) {
     console.log('Boa tarde!')
 } else {
     console.log('Boa noite!')
