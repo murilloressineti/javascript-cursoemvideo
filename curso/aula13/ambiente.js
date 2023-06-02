6da078dcd8219a4a088e0b5f3d8b6046ce72b630
murilloressineti <murillobr22@gmail.com>
2023-06-02 12:51:24 -0300

    Repetições

---
+++ curso/aula13/ambiente.js
@@ -0,0 +1,12 @@
+var cont = 1
+while (cont <= 6){
+    console.log(`Passo ${cont}`)
+    cont = cont + 1  //c++
+}
+
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c = c + 1
+} while (c <= 6)
---
+++ curso/aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for(var c = 1; c <= 3; c++) {
+    console.log(`Olá, seu cadastrado é o número ${c}`)
+}
+console.log('FIM!')
