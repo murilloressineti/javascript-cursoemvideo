2f0a86d25eaaf04d3dcb9c63ad3d26ab0db0c1f1
murilloressineti <murillobr22@gmail.com>
2023-06-06 12:24:22 -0300

    Exercício 18

--- curso/aula14ex/ex018/script.js
+++ curso/aula14ex/ex018/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function tabuada(){
+    let num = document.querySelector('#txtn')
+    let tab = document.querySelector('#seltab')
+
+    if (num.value.lenght == 0){
+        window.alert('Por favor digite um número')
+    } else {
+        let n = Number(num.value) //converte para número
+        let c = 1
+        tab.innerHTML = ''
+        while (c <=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
